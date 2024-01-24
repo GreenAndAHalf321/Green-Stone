6b40805ee2c0bd3c5a37140bb1b88eada2ae8499
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-01-24 11:51:38 +0100

    Create Colors.java

---
+++ Green Stone/src/main/java/at/foxel/greenstone/useful/Colors.java
@@ -0,0 +1,22 @@
+package at.foxel.greenstone.useful;
+
+public class Colors {
+
+    public final static String BLACK = "&0";
+    public final static String DARK_BLUE = "&1";
+    public final static String DARK_GREEN = "&2";
+    public final static String DARK_AQUA = "&3";
+    public final static String DARK_RED = "&4";
+    public final static String DARK_PURPLE = "&5";
+    public final static String GOLD = "&6";
+    public final static String GRAY = "&7";
+    public final static String DARK_GRAY = "&8";
+    public final static String BLUE = "&9";
+    public final static String GREEN = "&A";
+    public final static String AQUA = "&B";
+    public final static String RED = "&C";
+    public final static String LIGHT_PURPLE = "&D";
+    public final static String YELLOW = "&E";
+    public final static String WHITE = "&F";
+
+}
