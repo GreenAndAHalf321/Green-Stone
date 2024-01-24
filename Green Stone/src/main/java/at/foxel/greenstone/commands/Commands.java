aee2cfcd9544c2ffc329bd83afcc10a86ba460c8
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-01-24 10:24:51 +0100

    Added first log

--- Green Stone/src/main/java/at/foxel/greenstone/commands/Commands.java
+++ Green Stone/src/main/java/at/foxel/greenstone/commands/Commands.java
@@ -1,12 +1,16 @@
 package at.foxel.greenstone.commands;
 
+import at.foxel.greenstone.GreenStone;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Commands implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        GreenStone.getPluginLogger().info("Command " + label + " used");
+
         return false;
     }
 }
