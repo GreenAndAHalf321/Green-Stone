47e6651d8d77988cc5cc105332a25e1378a59d3b
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-01-24 10:32:28 +0100

    Added TODO

--- Green Stone/src/main/java/at/foxel/greenstone/commands/Commands.java
+++ Green Stone/src/main/java/at/foxel/greenstone/commands/Commands.java
@@ -1,15 +1,21 @@
 package at.foxel.greenstone.commands;
 
 import at.foxel.greenstone.GreenStone;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.*;
 
 public class Commands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
         GreenStone.getPluginLogger().info("Command " + label + " used");
+
+        //TODO Add settings to allow or disallow execution via command block, console or player
+        if(commandSender instanceof BlockCommandSender) {
+            GreenStone.getPluginLogger().info("Executed by Command Block");
+        }else if (commandSender instanceof ConsoleCommandSender) {
+            GreenStone.getPluginLogger().info("Executed by Console");
+        }else {
+            GreenStone.getPluginLogger().info("Executed by Player");
+        }
 
         return false;
     }
