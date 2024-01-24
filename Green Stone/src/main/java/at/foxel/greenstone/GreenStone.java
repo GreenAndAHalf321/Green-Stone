1cfc50fd0f26583b538aa9324de0b8b2ce9e13f4
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-01-24 12:14:23 +0100

    Registered command

--- Green Stone/src/main/java/at/foxel/greenstone/GreenStone.java
+++ Green Stone/src/main/java/at/foxel/greenstone/GreenStone.java
@@ -1,5 +1,6 @@
 package at.foxel.greenstone;
 
+import at.foxel.greenstone.commands.Commands;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -11,6 +12,13 @@ public final class GreenStone extends JavaPlugin {
     public void onEnable() {
         logger = getLogger();
         logger.info("You have been given the time stone. Use it wisely!!");
+
+        try {
+            this.getCommand("gs").setExecutor(new Commands());
+        } catch (NullPointerException e) {
+            logger.warning("Could not load commands");
+            getServer().getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
