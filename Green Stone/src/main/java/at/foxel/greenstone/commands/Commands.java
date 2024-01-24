fa6c8f3d8dfa685dc189cdf4bdf0229aad866f8d
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-01-24 11:26:52 +0100

    renamed commandSender to sender

--- Green Stone/src/main/java/at/foxel/greenstone/commands/Commands.java
+++ Green Stone/src/main/java/at/foxel/greenstone/commands/Commands.java
@@ -5,13 +5,13 @@ import org.bukkit.command.*;
 
 public class Commands implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         GreenStone.getPluginLogger().info("Command " + label + " used");
 
         //TODO Add settings to allow or disallow execution via command block, console or player
-        if(commandSender instanceof BlockCommandSender) {
+        if(sender instanceof BlockCommandSender) {
             GreenStone.getPluginLogger().info("Executed by Command Block");
-        }else if (commandSender instanceof ConsoleCommandSender) {
+        }else if (sender instanceof ConsoleCommandSender) {
             GreenStone.getPluginLogger().info("Executed by Console");
         }else {
             GreenStone.getPluginLogger().info("Executed by Player");
