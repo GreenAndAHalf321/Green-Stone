619aac830054b434db70d4f0556b6afd042f75dc
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-02-05 19:09:46 +0100

    Implemented block place listener

--- Green Stone/src/main/java/at/foxel/greenstone/listener/BlockListener.java
+++ Green Stone/src/main/java/at/foxel/greenstone/listener/BlockListener.java
@@ -5,10 +5,12 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class BlockListener implements Listener {
 
+    //Block break
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Bukkit.broadcastMessage("A block has been destroyed");
@@ -19,6 +21,12 @@ public class BlockListener implements Listener {
         for (Block block : event.blockList()) {
             Bukkit.broadcastMessage("A block has been destroyed by an explosion");
         }
+    }
+
+    //Block place
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Bukkit.broadcastMessage("A block has been placed");
     }
 
 }
