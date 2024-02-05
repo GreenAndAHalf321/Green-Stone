61038c50c5e9999941ede3ee2d09c2cb67709b0e
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-02-05 19:06:24 +0100

    Implemented block destroy listener

--- Green Stone/src/main/java/at/foxel/greenstone/listener/BlockListener.java
+++ Green Stone/src/main/java/at/foxel/greenstone/listener/BlockListener.java
@@ -1,15 +1,24 @@
 package at.foxel.greenstone.listener;
 
-import at.foxel.greenstone.GreenStone;
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class BlockListener implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
+        Bukkit.broadcastMessage("A block has been destroyed");
+    }
 
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event) {
+        for (Block block : event.blockList()) {
+            Bukkit.broadcastMessage("A block has been destroyed by an explosion");
+        }
     }
 
 }
