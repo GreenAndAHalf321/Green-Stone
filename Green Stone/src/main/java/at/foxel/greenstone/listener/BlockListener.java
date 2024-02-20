0dfccd082bb0c0512bba5702d3ce622168df9405
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-02-20 14:20:02 +0100

    Listeneers save data into recording

--- Green Stone/src/main/java/at/foxel/greenstone/listener/BlockListener.java
+++ Green Stone/src/main/java/at/foxel/greenstone/listener/BlockListener.java
@@ -1,5 +1,6 @@
 package at.foxel.greenstone.listener;
 
+import at.foxel.greenstone.Recording;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -15,45 +16,78 @@ public class BlockListener implements Listener {
     //Block break
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
+        if(!Recording.isRecording())
+            return;
+
+        Recording.getCurrentRecording().removeBlock(event.getBlock().getLocation());
         Bukkit.broadcastMessage("A block has been destroyed");
     }
 
     @EventHandler
     public void onEntityExplode(EntityExplodeEvent event) {
+        if(!Recording.isRecording())
+            return;
+
         for (Block block : event.blockList()) {
+            Recording.getCurrentRecording().removeBlock(block.getLocation());
             Bukkit.broadcastMessage("A block has been destroyed by an explosion");
         }
     }
 
     @EventHandler
     public void onBucketFill(PlayerBucketFillEvent event) {
+        if(!Recording.isRecording())
+            return;
+
+        Recording.getCurrentRecording().removeBlock(event.getBlock().getLocation());
         Bukkit.broadcastMessage("A liquid has been taken back");
     }
 
     @EventHandler
     public void onBlockFade(BlockFadeEvent event) {
+        if(!Recording.isRecording())
+            return;
+
+        if(event.getBlock().getType().isAir())
+            Recording.getCurrentRecording().removeBlock(event.getBlock().getLocation());
+        else
+            Recording.getCurrentRecording().addBlock(event.getBlock());
+
         Bukkit.broadcastMessage("A block was fading");
     }
 
     //Block place
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
+        if(!Recording.isRecording())
+            return;
 
         if(event.getBlockPlaced().getType().equals(Material.FIRE))
             return;
 
+        Recording.getCurrentRecording().addBlock(event.getBlock());
 
         Bukkit.broadcastMessage("A block has been placed");
     }
 
     @EventHandler
     public void onBucketEmpty(PlayerBucketEmptyEvent event) {
+        if(!Recording.isRecording())
+            return;
+
+        Recording.getCurrentRecording().addBlock(event.getBlock());
+
         Bukkit.broadcastMessage("A liquid has been placed");
     }
 
     //Block Swap (Liquid over snow for example)
     @EventHandler
     public void onBlockFromTo(BlockFromToEvent event) {
+        if(!Recording.isRecording())
+            return;
+
+        Recording.getCurrentRecording().addBlock(event.getBlock());
+
         if(event.getToBlock().getType().isAir()) {
             Bukkit.broadcastMessage("A liquid is spreading");
         } else {
@@ -64,11 +98,21 @@ public class BlockListener implements Listener {
     //Block spread (snow for example)
     @EventHandler
     public void onBlockForm(BlockFormEvent event) {
+        if(!Recording.isRecording())
+            return;
+
+        Recording.getCurrentRecording().addBlock(event.getBlock());
+
         Bukkit.broadcastMessage("A block is spreading");
     }
 
     @EventHandler
     public void onIgnite(BlockIgniteEvent event) {
+        if(!Recording.isRecording())
+            return;
+
+        Recording.getCurrentRecording().addBlock(event.getBlock());
+
         Bukkit.broadcastMessage("A block ignited");
     }
 
