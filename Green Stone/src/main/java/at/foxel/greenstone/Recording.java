de57da79dfe8a89a9a4aa2c30e208446bba94ec8
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-02-20 12:30:08 +0100

    Added timer

--- Green Stone/src/main/java/at/foxel/greenstone/Recording.java
+++ Green Stone/src/main/java/at/foxel/greenstone/Recording.java
@@ -6,15 +6,34 @@ import org.bukkit.block.Block;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Recording {
     private final Queue<BlockState> blockUpdates = new ConcurrentLinkedQueue<>();
-    private final LinkedList<BlockState> states = new LinkedList<>();
+    private final LinkedList<WorldState> worldStates = new LinkedList<>();
     private final String name;
 
-    public Recording(String name) {
+    private final int timeIntervalInMilliseconds;
+
+    private Timer timer = new Timer();
+    private TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            BlockState currentState;
+            WorldState worldState = new WorldState();
+            while (!blockUpdates.isEmpty()) {
+                currentState = blockUpdates.poll();
+                worldState.addBlockState(currentState);
+            }
+            worldStates.add(worldState);
+        }
+    };
+
+    public Recording(String name, int timeIntervalInMilliseconds) {
         this.name = name;
+        this.timeIntervalInMilliseconds = timeIntervalInMilliseconds;
     }
 
     public void addBlock(Block block) {
@@ -25,8 +44,8 @@ public class Recording {
         blockUpdates.add(new BlockState(Material.AIR, location));
     }
 
-    public LinkedList<BlockState> getStates() {
-        return (LinkedList<BlockState>) states.clone();
+    public LinkedList<WorldState> getWorldStates() {
+        return (LinkedList<WorldState>) worldStates.clone();
     }
 
     public String getName() {
