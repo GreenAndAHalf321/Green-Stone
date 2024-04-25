31d0e2dcefccbb5a3df6c144e2d7359716dacb8c
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-04-25 11:44:02 +0200

    Added InventoryClickEvent

--- Green Stone/src/main/java/at/foxel/greenstone/listener/InventoryListener.java
+++ Green Stone/src/main/java/at/foxel/greenstone/listener/InventoryListener.java
@@ -1,4 +1,25 @@
 package at.foxel.greenstone.listener;
 
-public class InventoryListener {
+import at.foxel.greenstone.useful.Colors;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class InventoryListener implements Listener {
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        //TODO Use somthing else instead of the String title
+        if (!event.getView().getTitle().equals("Playbacks"))
+            return;
+
+        //TODO start playback
+
+        HumanEntity playerWhoClicked = event.getWhoClicked();
+        playerWhoClicked.sendMessage(Colors.GREEN + "You started the playback " + Colors.YELLOW
+                + event.getCursor().getItemMeta().getDisplayName());
+        playerWhoClicked.closeInventory();
+        event.setCancelled(true);
+    }
 }
