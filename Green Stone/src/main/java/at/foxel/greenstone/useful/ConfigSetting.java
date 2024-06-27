ebb1ca076131e5af7f1348f880ed1929203ed641
Florian Wallner <80975675+GreenAndAHalf321@users.noreply.github.com>
2024-06-27 18:16:00 +0200

    Changed getters name

--- Green Stone/src/main/java/at/foxel/greenstone/useful/ConfigSetting.java
+++ Green Stone/src/main/java/at/foxel/greenstone/useful/ConfigSetting.java
@@ -93,7 +93,7 @@ public class ConfigSetting {
         return material;
     }
 
-    public Object isDefaultSetting() {
+    public Object getDefaultSetting() {
         return defaultSetting;
     }
 
